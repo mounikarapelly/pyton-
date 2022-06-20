3da954d4983540429a3b8750a5ed38f260ea63ff
mounikarapelly <99536475+mounikarapelly@users.noreply.github.com>
2022-06-20 21:05:33 +0530

    Add files via upload

---
+++ passkeyword/string.py
@@ -0,0 +1,6 @@
+pystr ="MACHINE LEARNING"
+print(pystr)
+print(pystr[0:2])
+print(pystr[2:5])
+print(pystr[5:7])
+print(pystr[9:13])
---
+++ passkeyword/string1.py
@@ -0,0 +1,6 @@
+pystr="MACHINE LEARNING"
+print(pystr)
+print(pystr[0])
+print(pystr[-2])
+print(pystr[-6])
+print(pystr[6])
